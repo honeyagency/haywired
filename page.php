740760986f5ca39e0f808f1df4614848b8d102a8
joshre <joshre@me.com>
2018-04-18 11:30:28 -0700

    Resiliency

--- page.php
+++ page.php
@@ -45,8 +45,6 @@ if (is_front_page()) {
     $context['events'] = getCustomPosts('events', -1, null, 'date', null, null);
 } elseif (is_page(134)) {
     $context['reports'] = prepareReportsPageFields();
-} elseif (is_page(497)) {
-    $context['challenge'] = challengePageOne();
 } elseif (is_page(626)) {
     $context['challenge'] = challengePageOne();
 }
